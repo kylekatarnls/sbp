fb166d553c5f64fc023dc6aa3d984d76f48f817f
kylekatarnls <kylekatarnls@gmail.com>
2014-02-19 23:03:05 +0100

    Corrections namespace

--- src/sbp/laravel/ClassLoader.php
+++ src/sbp/laravel/ClassLoader.php
@@ -1,19 +1,38 @@
-<?php namespace sbp\laravel
+<?php namespace sbp\laravel;
 
-class ClassLoader extends Illuminate\Support\ClassLoader
+class ClassLoader extends \Illuminate\Support\ClassLoader
 {
+	/**
+	 * Load the given class file.
+	 *
+	 * @param  string  $class
+	 * @return void
+	 */
 	public static function load($class)
 	{
 		$class = static::normalizeClass($class);
 
 		foreach (static::$directories as $directory)
 		{
-			if (\sbp::fileExists($path = $directory.DIRECTORY_SEPARATOR.$class))
+			if (\sbp\sbp::fileExists($path = $directory.DIRECTORY_SEPARATOR.$class))
 			{
 				require_once $path;
 
 				return true;
 			}
+		}
+	}
+
+	/**
+	 * Register the given class loader on the auto-loader stack.
+	 *
+	 * @return void
+	 */
+	public static function register($prepend = true)
+	{
+		if ( ! static::$registered)
+		{
+			static::$registered = spl_autoload_register(array('\sbp\laravel\ClassLoader', 'load'), true, $prepend);
 		}
 	}
 }
