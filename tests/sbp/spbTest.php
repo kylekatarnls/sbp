01ff14d50730e0e69a3ad5f5e42460b687b8f11c
Bastien <kylekatarnls@github.com>
2014-02-22 21:33:10 +0100

    Syntaxe test method

--- tests/sbp/spbTest.php
+++ tests/sbp/spbTest.php
@@ -5,9 +5,20 @@ use sbp\sbpException;
 
 class sbpTest extends \PHPUnit_Framework_TestCase
 {
+	protected function assertParse($from, $to, $message = null)
+	{
+		if(is_null($message))
+		{
+			$message = "sbp::parse(\"$from\") do not return \"$to\"";
+		}
+		$from = str_replace(array("\n", "\r", "\t", ' '), '', trim(sbp::parse($from)));
+		$to = str_replace(array("\n", "\r", "\t", ' '), '', trim($to));
+		return $this->assertTrue($from === $to, $message);
+	}
+
 	public function parseTest()
 	{
-		$this->assertEquals(sbp::parse("ANameSpace\\BClass:CNameSpace\\DClass"), "class ANameSpace\\BClass extends CNameSpace\\DClass {}");
+		$this->assertParse("ANameSpace\\BClass:CNameSpace\\DClass", "class ANameSpace\\BClass extends CNameSpace\\DClass {}");
 	}
 }
 
