92a187021d0a14418ca079a0df4ed4557beb2979
Bastien <kylekatarnls@github.com>
2014-03-13 16:32:11 +0100

    Catch correction

--- src/sbp/sbp.php
+++ src/sbp/sbp.php
@@ -540,7 +540,8 @@ namespace sbp
 				'#(?<![a-zA-Z0-9_\x7f-\xff\$])function\s*(array\s.+|[A-Z\$\&].+)?\s*\{#U' => 'function ($1) {',
 				'#(?<![a-zA-Z0-9_\x7f-\xff\$])function\s+use(?![a-zA-Z0-9_\x7f-\xff])#U' => 'function () use',
 				'#(?<![a-zA-Z0-9_\x7f-\xff\$])(function.*[^a-zA-Z0-9_\x7f-\xff\$])use\s*((array\s.+|[A-Z\$].+)\{)#U' => '$1 ) use ( $2',
-
+				'#\((\([^\(\)]+\))\)#' => '$1',
+				'#(catch\s*\([^\)]+\)\s*)([^\s\{])' => '$1{} $2',
 			) as $from => $to) {
 				$content = preg_replace($from, $to, $content);
 			}
