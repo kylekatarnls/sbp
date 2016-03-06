a4682437ef8e6b927ba9579ad90ff8dbdbf001bf
Kyle <kylekatarnls@gmail.com>
2016-03-06 23:26:41 +0100

    Fix style

--- sbp.php
+++ sbp.php
@@ -7,7 +7,8 @@ use Sbp\Sbp;
 define('CREATE_TEST', 'create-test');
 define('COMPILE_TEST', 'compile-test');
 
-function argn($i) {
+function argn($i)
+{
     global $argv;
 
     return isset($argv[$i]) ? $argv[$i] : null;
@@ -18,7 +19,7 @@ $script = argn(0);
 switch ($command = argn(1)) {
     case CREATE_TEST:
         $name = argn(2);
-        $path = __DIR__ . '/tests/sbp/files/.src/' . $name . '.php';
+        $path = __DIR__.'/tests/sbp/files/.src/'.$name.'.php';
         touch($path);
         $path = realpath($path);
         echo "You can now edit the SBP test file:\n";
@@ -30,12 +31,12 @@ switch ($command = argn(1)) {
     case COMPILE_TEST:
         $name = argn(2);
         Sbp::fileParse(
-            __DIR__ . '/tests/sbp/files/.src/' . $name . '.php',
-            __DIR__ . '/tests/sbp/files/' . $name . '.php'
+            __DIR__.'/tests/sbp/files/.src/'.$name.'.php',
+            __DIR__.'/tests/sbp/files/'.$name.'.php'
         );
 
     default:
         echo "Usage:\n  php $script <command>\n\n";
-        echo "Commands:\n  " . CREATE_TEST . "\n  " . COMPILE_TEST;
+        echo "Commands:\n  ".CREATE_TEST."\n  ".COMPILE_TEST;
         break;
 }
