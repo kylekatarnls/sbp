cf57c9aa3b335945d4925c089a27ac12756b6ca0
Bastien Miclo <bmiclo@novahealthsa.com>
2016-03-07 17:25:24 +0100

    Fix regex replacement

--- tests/sbp/files/.src/super_methods.php
+++ tests/sbp/files/.src/super_methods.php
@@ -15,3 +15,19 @@ echo {
 	tata = 5
 	lulu = 6
 }-->keys()-->implode(', ') . "\n"
+
+echo "AbcdEF"-->replace(/[a-z]/, '#') . "\n"
+
+echo [1, 3, 7]-->sum() . "\n"
+
+f array_custom($array, $a, $b)
+    $array[] = $a+$b
+    < $array
+
+f str_custom($string, $a, $b)
+    $string .= $a+$b
+    < $string
+
+print_r([5]-->custom(1, 2))
+
+echo "abc"-->custom(1, 2) . "\n"
