713ae0a86deadb21b20e987056140c86460b5f9a
kylekatarnls <sbp@selfbuild.fr>
2016-03-09 09:19:18 +0100

    Test isSbp

--- tests/sbp/features/utilsTest.php
+++ tests/sbp/features/utilsTest.php
@@ -8,11 +8,11 @@ class UtilsTest extends TestCompileCase
 	public function testIsSbp()
 	{
         $tmp = $this->getTmp();
-        copy(__DIR__.'/../files/.src/return.php', $tmp.'/_return.sbp.php');
-        copy(__DIR__.'/../../bootstrap.php', $tmp.'/_bootstrap.sbp.php');
-        sbp($tmp.'/_return');
-		$this->assertFalse(Sbp::isSbp($tmp.'/_bootstrap.sbp.php'));
-		$this->assertFalse(Sbp::isSbp($tmp.'/_return.sbp.php'));
-		$this->assertTrue(Sbp::isSbp($tmp.'/__return.sbp.php'));
+        copy(__DIR__.'/../files/.src/return.php', $tmp.'_return.sbp.php');
+        copy(__DIR__.'/../../bootstrap.php', $tmp.'_bootstrap.sbp.php');
+        Sbp::fileParse($tmp.'_return.sbp.php', $tmp.'__return.sbp.php');
+		$this->assertFalse(Sbp::isSbp($tmp.'_bootstrap.sbp.php'));
+		$this->assertFalse(Sbp::isSbp($tmp.'_return.sbp.php'));
+		$this->assertTrue(Sbp::isSbp($tmp.'__return.sbp.php'));
 	}
 }
