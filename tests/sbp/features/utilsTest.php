b97556f85c6a6b8ea879312b6538d5aa17072fea
kylekatarnls <sbp@selfbuild.fr>
2016-03-09 13:19:00 +0100

    Test file parse exceptions and file helper

--- tests/sbp/features/utilsTest.php
+++ tests/sbp/features/utilsTest.php
@@ -1,7 +1,45 @@
 <?php
 
+use Sbp\Plugins\Helpers\FileHelper;
 use Sbp\Sbp;
+use Sbp\SbpException;
 use Sbp\Wrapper\TestCompileCase;
+
+class FileEmulation
+{
+    function url_stat($path)
+    {
+        $mode = 0666;
+        $uid = 7;
+        $gid = 8;
+
+        $len = strlen('fiemulate://');
+        $type = substr($path, $len, 1);
+        switch (substr($path, $len, 1)) {
+            case 'u':
+                $uid = getmyuid();
+                break;
+            case 'g':
+                $gid = getmyuid();
+                break;
+            case 'a':
+                $uid = getmyuid();
+                $gid = getmyuid();
+                break;
+        }
+        switch (substr($path, $len + 2)) {
+            case 'not_readable':
+                $mode = 0222;
+                break;
+            case 'not_writable':
+                $mode = 0444;
+                break;
+        }
+
+        return array(1, 2, $mode, 3, $uid, $gid, 4, 1, 1, 1, 1, 5, 6);
+    }
+}
+
 
 class UtilsTest extends TestCompileCase
 {
@@ -14,5 +52,43 @@ class UtilsTest extends TestCompileCase
 		$this->assertFalse(Sbp::isSbp($tmp.'_bootstrap.sbp.php'));
 		$this->assertFalse(Sbp::isSbp($tmp.'_return.sbp.php'));
 		$this->assertTrue(Sbp::isSbp($tmp.'__return.sbp.php'));
+	}
+
+	public function testFileHelper()
+	{
+        $tmp = $this->getTmp();
+        copy(__DIR__.'/../files/.src/return.php', $tmp.'_return.sbp.php');
+        copy(__DIR__.'/../../bootstrap.php', $tmp.'_bootstrap.sbp.php');
+        Sbp::fileParse($tmp.'_return.sbp.php', $tmp.'__return.sbp.php');
+        $source = FileHelper::getSbpSource($tmp.'__return.sbp.php');
+        $this->assertTrue(file_exists($source));
+        $this->assertSame($source, FileHelper::cleanPath($tmp.'_return.sbp.php'));
+	}
+
+	protected function emulateFile($from, $to, $expected)
+	{
+        if (!in_array('fiemulate', stream_get_wrappers())) {
+            stream_wrapper_register('fiemulate', 'FileEmulation');
+        }
+        $message = '';
+        try {
+            Sbp::fileParse('fiemulate://'.$from, 'fiemulate://'.$to);
+        } catch (SbpException $e) {
+            $message = $e->getMessage();
+        }
+        $this->assertTrue(strpos($message, 'chmod '.$expected) !== false, 'The following message does not contain "chmod '.$expected.'":'."\n".$message);
+	}
+
+	public function testFileParseExceptions()
+	{
+        $this->emulateFile('fiemulate://u_not_readable', 'fiemulate://open_dir/file', 'u+r');
+        // if unix
+        if (getmyuid()) {
+            $this->emulateFile('fiemulate://g_not_readable', 'fiemulate://open_dir/file', 'g+r');
+            $this->emulateFile('fiemulate://o_not_readable', 'fiemulate://open_dir/file', 'o+r');
+            $this->emulateFile('fiemulate://a_openfile', 'fiemulate://u_not_writable/file', 'u+w');
+            $this->emulateFile('fiemulate://a_openfile', 'fiemulate://g_not_writable/file', 'g+w');
+            $this->emulateFile('fiemulate://a_openfile', 'fiemulate://o_not_writable/file', 'o+w');
+        }
 	}
 }
