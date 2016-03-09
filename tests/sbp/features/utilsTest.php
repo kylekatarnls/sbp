046905a995d39ff4757b0d69500d3748461e8972
kylekatarnls <sbp@selfbuild.fr>
2016-03-09 08:40:24 +0100

    Test isSbp

---
+++ tests/sbp/features/utilsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Sbp\Sbp;
+use Sbp\Wrapper\TestCompileCase;
+
+class UtilsTest extends TestCompileCase
+{
+	public function testIsSbp()
+	{
+        $tmp = $this->getTmp();
+        copy(__DIR__.'/../files/.src/return.php', $tmp.'/_return.sbp.php');
+        copy(__DIR__.'/../../bootstrap.php', $tmp.'/_bootstrap.sbp.php');
+        sbp($tmp.'/_return');
+		$this->assertFalse(Sbp::isSbp($tmp.'/_bootstrap.sbp.php'));
+		$this->assertFalse(Sbp::isSbp($tmp.'/_return.sbp.php'));
+		$this->assertTrue(Sbp::isSbp($tmp.'/__return.sbp.php'));
+	}
+}
