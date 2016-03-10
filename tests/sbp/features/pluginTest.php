6a80c437bb0c6d0a337b5407b22ceb31db4fd114
kylekatarnls <sbp@selfbuild.fr>
2016-03-10 11:23:23 +0100

    Exceptions unit tests

--- tests/sbp/features/pluginTest.php
+++ tests/sbp/features/pluginTest.php
@@ -5,6 +5,17 @@ use Sbp\Wrapper\TestCase;
 
 class PluginTest extends TestCase
 {
+	/**
+	 * @after
+	 */
+	public function removeAllPlugins()
+	{
+		sbp_remove_plugin('jQuery');
+		sbp_remove_plugin('foo1');
+		sbp_remove_plugin('foo2');
+		sbp_remove_plugin('foo3');
+	}
+
 	public function testAddPlugin()
 	{
 		sbp_add_plugin('jQuery', '$(', 'new jQuery(');
@@ -31,5 +42,33 @@ class PluginTest extends TestCase
 	public function testPluginArgumentsError()
 	{
 		sbp_add_plugin('Foo', array(), 'bar');
+	}
+
+	public function testPluginNotCalled()
+	{
+		sbp_add_plugin('foo1', '`foo`', function () {
+			throw new \Exception("Error Processing Request", 1);
+		});
+		$this->assertParse("\$bar = 1\necho \$bar", "\$bar = 1;\necho \$bar;");
+	}
+
+	/**
+	 * @expectedException Exception
+	 */
+	public function testPluginWithException()
+	{
+		sbp_add_plugin('foo2', '`foo`', function () {
+			throw new \Exception("Error Processing Request", 1);
+		});
+		Sbp::parse("<?\n\$foo = 1\necho \$foo");
+	}
+
+	/**
+	 * @expectedException Exception
+	 */
+	public function testPluginNotCallable()
+	{
+		sbp_add_plugin('foo3', 'not_callable');
+		Sbp::parse("<?\necho 'Hello'");
 	}
 }
