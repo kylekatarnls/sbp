da828af8fe2a81968ff4ee11271a7a6c4af843e9
kylekatarnls <sbp@selfbuild.fr>
2016-03-10 10:40:53 +0100

    Production method unit test

---
+++ tests/sbp/features/prodModeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Sbp\Sbp;
+use Sbp\Wrapper\TestCompileCase;
+
+class ProdModeTest extends TestCompileCase
+{
+	public function testSuperMethods()
+	{
+        $tmp = $this->getTmp();
+        Sbp::prod();
+		copy(__DIR__.'/../files/.src/return.php', $tmp.'_return_prod.sbp.php');
+		file_put_contents($tmp.'__return_prod.php', '<?php return 1138;');
+        touch($tmp.'__return_prod.php', time() - 3600);
+        $this->assertSame(1138, sbp_include_once($tmp.'_return_prod'));
+        $this->assertSame(1138, sbp($tmp.'_return_prod'));
+        Sbp::dev();
+		copy(__DIR__.'/../files/.src/return.php', $tmp.'_return_dev.sbp.php');
+		file_put_contents($tmp.'__return_dev.php', '<?php return 1138;');
+        touch($tmp.'__return_dev.php', time() - 3600);
+        $this->assertSame(42, sbp_include_once($tmp.'_return_dev'));
+        $this->assertSame(42, sbp($tmp.'_return_dev'));
+	}
+}
